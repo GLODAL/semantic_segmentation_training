1399eb6e85c350b09ddf68797f3e7d0be374d1e7
heromiya <heromiya@heromiya.net>
2023-05-02 17:26:30 +0700

    new file:   Prachuap A, Year 2018.tif 	new file:   Prachuap A, Year 2020.tif 	new file:   Prachuap B, Year 2018.tif 	new file:   Prachuap B, Year 2020.tif 	new file:   demo_model.hdf5     new file:   demo_rubber.zip 	modified:   merge_annotation_for_testing.sh 	modified:   split_images_for_testing.sh

--- merge_annotation_for_testing.sh
+++ merge_annotation_for_testing.sh
@@ -8,21 +8,21 @@ export OUTPUT=$2
 IFS='
 '
 
-for IMG_EXTENDED in $(find $PAT_IMG_DIR -type f -regex ".*tif$"); do
+for PAT_IMG in $(find $PAT_IMG_DIR -type f -regex ".*tif$"); do
     IFS=' '
-    JSON=$(gdalinfo -json "$IMG_EXTENDED")
+    JSON=$(gdalinfo -json "$PAT_IMG")
     SIZE=($(echo $JSON | python3 -c "import sys, json; print(json.load(sys.stdin)['size'])" | tr -d [],))
     X_SIZE=${SIZE[0]}
     Y_SIZE=${SIZE[1]}
 
-    PRED_TIF=$PAT_PRD_DIR/$(basename "$IMG_EXTENDED").pred.tif
+    PAT_PRD=$PAT_PRD_DIR/$(basename "$PAT_IMG").pred.tif
 
     upperLeft=($(echo $JSON | python3 -c "import sys, json; print(json.load(sys.stdin)['cornerCoordinates']['upperLeft'])" | tr -d [],))
     lowerRight=($(echo $JSON | python3 -c "import sys, json; print(json.load(sys.stdin)['cornerCoordinates']['lowerRight'])" | tr -d [],))
 
-    gdal_translate -q -ot Byte -a_srs EPSG:3857 -a_ullr ${upperLeft[0]} ${upperLeft[1]} ${lowerRight[0]} ${lowerRight[1]} "$PRED_TIF" "$TEST_RESULTS/$(basename "$PRED_TIF").g.tif"
+    gdal_translate -q -ot Byte -a_srs EPSG:3857 -a_ullr ${upperLeft[0]} ${upperLeft[1]} ${lowerRight[0]} ${lowerRight[1]} "$PAT_PRD" "$PAT_PRD.g.tif"
 done
 
-gdalwarp -co COMPRESS=Deflate $PAT_PRD_DIR/*.tif $OUTPUT
+gdalwarp -co COMPRESS=Deflate $PAT_PRD_DIR/*.g.tif $OUTPUT
 
 exit 0
--- split_images_for_testing.sh
+++ split_images_for_testing.sh
@@ -3,18 +3,18 @@
 export INPUT_GTIFF="$1"
 export WORKDIR=$2
 
-export TEST_PATCH_SIZE=$2
+export TEST_PATCH_SIZE=$3
 
 export PAT_IMG_DIR=$WORKDIR/pat_img/
 mkdir -p $PAT_IMG_DIR
 
 function subset_patch_image (){
-  IN=$1
+  IN="$1"
   X=$2
   Y=$3
   XOFF=$(perl -e "print (($X - 1) * $TEST_PATCH_SIZE)")
   YOFF=$(perl -e "print (($Y - 1) * $TEST_PATCH_SIZE)")
-  gdal_translate -q -srcwin $XOFF $YOFF $TEST_PATCH_SIZE $TEST_PATCH_SIZE $IN "$PAT_IMG_DIR/$(basename "$IN")-i-$X-$Y.tif"
+  gdal_translate -q -srcwin $XOFF $YOFF $TEST_PATCH_SIZE $TEST_PATCH_SIZE "$IN" "$PAT_IMG_DIR/$(basename "$IN")-i-$X-$Y.tif"
 }
 export -f subset_patch_image
 
@@ -26,8 +26,10 @@ N_PATCH_X=$(perl -e "use POSIX qw(floor ceil); print ceil($X_SIZE / $TEST_PATCH_
 N_PATCH_Y=$(perl -e "use POSIX qw(floor ceil); print ceil($Y_SIZE / $TEST_PATCH_SIZE)")  
 X_SIZE_EXTEND=$(expr $N_PATCH_X \* $TEST_PATCH_SIZE)
 Y_SIZE_EXTEND=$(expr $N_PATCH_Y \* $TEST_PATCH_SIZE)
-gdal_translate -q -srcwin 0 0 $X_SIZE_EXTEND $Y_SIZE_EXTEND "$INPUT_GTIFF" $WORKDIR/$(basename "$INPUT_GTIFF")
+
+EXTENDED_TIF="$WORKDIR/$(md5sum "$INPUT_GTIFF" | cut -f 1 -d ' ').tif"
+gdal_translate -q -srcwin 0 0 $X_SIZE_EXTEND $Y_SIZE_EXTEND "$INPUT_GTIFF" $EXTENDED_TIF
 IFS='
 '
-#  for X in $(seq 1 10); do for Y in $(seq 1 10); do subset_patch_image $WORKDIR/$(basename "$TIF") $X $Y; done; done
-parallel subset_patch_image $WORKDIR/$(basename "$TIF") {} {} ::: $(seq 1 $N_PATCH_X) ::: $(seq 1 $N_PATCH_Y)
+#for X in $(seq 1 $N_PATCH_X); do for Y in $(seq 1 $N_PATCH_Y); do subset_patch_image "$WORKDIR/$(basename "$INPUT_GTIFF")" $X $Y; done; done
+parallel subset_patch_image $EXTENDED_TIF {} {} ::: $(seq 1 $N_PATCH_X) ::: $(seq 1 $N_PATCH_Y)
